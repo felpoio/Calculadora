c49183cffce2d5c9d782dc43940c8ac8857b6a38
felpo <feliperangel897@gmail.com>
2023-12-06 20:55:03 -0300

    add calculator logic

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 let num1
-let num2
-let operador
+let num2 
+let operator
+let displayValue = document.querySelector('#display')
+let clear = document.querySelector('#clear')
+let equals = document.querySelector('#equals')
 
-function operate(operador, num1, num2) {
-    
+function mostraDisplay(elem) {
+    displayValue.textContent += elem.innerText
+    console.log(elem.innerText)
+}
+
+function selecionaOperador(elem) {
+    num1 = displayValue.innerText
+    displayValue.innerText= ''
+    operator = elem.innerText
+}
+
+equals.addEventListener('click', () => {
+    num2 = displayValue.innerText
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    operate(operator, num1, num2)
+})
+
+clear.addEventListener('click', () => displayValue.innerText =  '')
+
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        displayValue.innerText = num1 + num2
+    } else if (operator === '-'){
+        displayValue.innerText = num1 - num2
+    } else if (operator === 'x'){
+        displayValue.innerText = num1 * num2
+    } else if (operator === '/'){
+        displayValue.innerText = num1 / num2
+    }
 }
